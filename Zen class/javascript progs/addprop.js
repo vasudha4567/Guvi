e413cac3b1ee5eff9bca06e1c22684d672689139
vasudha4567 <45098390+vasudha4567@users.noreply.github.com>
2019-12-12 15:22:30 +0530

    Add files via upload

---
+++ Zen class/javascript progs/addprop.js
@@ -0,0 +1,7 @@
+var obj1={}
+key="myproperty"
+function addProperty(obj1, key){
+obj1[key]=true
+console.log(obj1)
+}
+addProperty(obj1,key)
---
+++ Zen class/javascript progs/getprop.js
@@ -0,0 +1,6 @@
+var j={k1:"cat"}
+function getProperty(obj, key) {
+var a=Object.values(j)
+console.log(a)
+}
+getProperty(j)
